30d4d211d2a285f4889d0cea28a4b4ba67883d1b
Dmitry Volkov <85643807+mariadb-DmitryVolkov@users.noreply.github.com>
2021-11-05 21:30:00 -0700

    + load_yaml_config

--- xpand_locust/__init__.py
+++ xpand_locust/__init__.py
@@ -1,2 +1,2 @@
 from .custom_locust import CustomClient, CustomLocust, CustomTasks
-from .locust_utils import load_seed_file
+from .locust_utils import load_seed_file, load_yaml_config, YamlConfigException
