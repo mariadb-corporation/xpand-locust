74ba0e4e3c01945351d2961f9dc1d26c4c84f824
Dmitry Volkov <85643807+mariadb-DmitryVolkov@users.noreply.github.com>
2021-12-08 08:12:51 -0800

    + bug fixes for setup envs

--- bin/pyenv_setup.sh
+++ bin/pyenv_setup.sh
@@ -9,13 +9,11 @@ rm -rf ${PYENV_ROOT}
 # From https://github.com/pyenv/pyenv-installer
 curl https://pyenv.run | bash
 
-# Todo update bashrc only if pyenv not in path already
-#if command -v pyenv 1>/dev/null 2>&1; then
-#  eval "$(pyenv init -)"
-#fi
-
-echo 'export PYENV_ROOT="$HOME/.pyenv"' >>~/.bashrc
-echo 'export PATH="$PYENV_ROOT/bin:$PATH"' >>~/.bashrc
-echo 'eval "$(pyenv init --path)"' >>~/.bashrc
+# update bashrc only if pyenv not in path already
+if ! command -v pyenv 1>/dev/null 2>&1; then
+    echo 'export PYENV_ROOT="$HOME/.pyenv"' >>~/.bashrc
+    echo 'export PATH="$PYENV_ROOT/bin:$PATH"' >>~/.bashrc
+    echo 'eval "$(pyenv init --path)"' >>~/.bashrc
+fi
 
 echo "Done with pyenv"
