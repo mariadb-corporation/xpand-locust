a0bd4ee0cf4fc8754d7a0355d44965b5034bb6a5
Dmitry Volkov <dmitry.volkov@mariadb.com>
2022-11-04 16:09:23 -0700

    + shapes

--- use_cases/sysbench/locustfile_simple.py
+++ use_cases/sysbench/locustfile_simple.py
@@ -9,6 +9,8 @@
 import numpy
 from locust import between, constant, constant_throughput, task
 from xpand_locust import CustomLocust, CustomTasks
+from locust import LoadTestShape
+
 
 TOTAL_ROWS = 1000000  # Number of rows per table
 BULK_ROWS = 100  # how many rows to use for range scan
@@ -118,3 +120,35 @@ class MyUser(CustomLocust):
 
     tasks = [MyTasks]
     wait_time = constant(0)  # constant_throughput(50)  # between(0.01, 0.05)
+
+
+class StagesShape(LoadTestShape):
+    """
+    A simply load test shape class that has different user and spawn_rate at
+    different stages.
+    Keyword arguments:
+        stages -- A list of dicts, each representing a stage with the following keys:
+            duration -- When this many seconds pass the test is advanced to the next stage
+            users -- Total user count
+            spawn_rate -- Number of users to start/stop per second
+            stop -- A boolean that can stop that test at a specific stage
+        stop_at_end -- Can be set to stop once all stages have run.
+    """
+
+    stages = [
+        {"duration": 120, "users": 256, "spawn_rate": 64},
+        {"duration": 120, "users": 384, "spawn_rate": 64},
+        {"duration": 120, "users": 512, "spawn_rate": 10},
+        {"duration": 120, "users": 640, "spawn_rate": 10},
+        {"duration": 120, "users": 1024, "spawn_rate": 10},
+    ]
+
+    def tick(self):
+        run_time = self.get_run_time()
+
+        for stage in self.stages:
+            if run_time < stage["duration"]:
+                tick_data = (stage["users"], stage["spawn_rate"])
+                return tick_data
+
+        return None
